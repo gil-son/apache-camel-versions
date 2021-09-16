65fa289c0e4147e6aed2541adcacf8ad9cc9ee1b
“gil-son” <gilszon@gmail.com>
2021-09-16 19:44:04 -0300

    Send Message to MQ Queue

---
+++ send_and_consume_mqqueue_and_send_andpoint/sendmessagetomqqueue/src/main/java/com/sendmessagetomqqueue/SendmessagetomqqueueApplication.java
@@ -0,0 +1,13 @@
+package com.sendmessagetomqqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmessagetomqqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendmessagetomqqueueApplication.class, args);
+	}
+
+}
