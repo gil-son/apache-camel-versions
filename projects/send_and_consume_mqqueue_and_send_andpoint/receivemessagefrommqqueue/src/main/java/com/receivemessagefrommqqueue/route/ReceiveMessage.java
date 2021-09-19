a617ee59e63e3c1d092d2572a34172fa2bdb0e06
“gil-son” <gilszon@gmail.com>
2021-09-19 20:55:15 -0300

    Configuring the ActiveMQ Receiver Route

--- projects/send_and_consume_mqqueue_and_send_andpoint/receivemessagefrommqqueue/src/main/java/com/receivemessagefrommqqueue/route/ReceiveMessage.java
+++ projects/send_and_consume_mqqueue_and_send_andpoint/receivemessagefrommqqueue/src/main/java/com/receivemessagefrommqqueue/route/ReceiveMessage.java
@@ -1,19 +1,42 @@
 package com.receivemessagefrommqqueue.route;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import com.receivemessagefrommqqueue.model.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class ReceiveMessage extends RouteBuilder{
 
+	
+	//@Autowired
+	GetEmployee getEmployee;
+	
+	
+	ReceiveMessage(GetEmployee getEmployee){
+		this.getEmployee = getEmployee;
+	}
+	
+	
 	@Override
 	public void configure() throws Exception {
 		
 		from("activemq:test-activemq-queue")
-		.log("Message ${body}")
-		.to("log:com.receivemessagefrommqqueue?level=TRACE&showAll=true&multiline=true");
+		.unmarshal().json(JsonLibrary.Jackson, Employee.class)
+		.bean(getEmployee)
+		.to("log:myloggingqueue");
 		
-	
 	}
-
+	
+	
+	@Component
+	class GetEmployee{
+	    Logger logger= LoggerFactory.getLogger(GetEmployee.class);
+	    public void getData(Employee employee){
+	        logger.info("Emp data: "+employee.getId());
+	    }
+	}
 }
