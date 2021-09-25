c32fee621b9b9a48edb05b07322fb075a8cfc6f1
“gil-son” <gilszon@gmail.com>
2021-09-25 12:58:58 -0300

    Create endpoint

--- projects/send_and_consume_mqqueue_and_send_endpoint/receivemessagefrommqqueue/src/main/java/com/receivemessagefrommqqueue/route/ReceiveMessage.java
+++ projects/send_and_consume_mqqueue_and_send_endpoint/receivemessagefrommqqueue/src/main/java/com/receivemessagefrommqqueue/route/ReceiveMessage.java
@@ -2,14 +2,15 @@ package com.receivemessagefrommqqueue.route;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.dataformat.JsonLibrary;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import com.receivemessagefrommqqueue.model.Employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @Component
-
 public class ReceiveMessage extends RouteBuilder{
 
 	
@@ -27,6 +28,11 @@ public class ReceiveMessage extends RouteBuilder{
 		.to("log:myloggingqueue");
 		
 		
+		
+		restConfiguration().component("servlet").port(9000).host("localhost").bindingMode(RestBindingMode.json);
+		
+		rest().get("/test").produces(MediaType.APPLICATION_JSON_VALUE).route()
+			.setBody(constant("Test message"));
 	}
 	
 	
