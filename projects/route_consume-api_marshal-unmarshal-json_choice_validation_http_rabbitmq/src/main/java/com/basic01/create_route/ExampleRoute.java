5029d6735eba2c1ac8f3384a157ab118f0e5e4ae
“gil-son” <gilszon@gmail.com>
2021-09-26 19:18:37 -0300

    First Message

---
+++ projects/route_consume-api_marshal-unmarshal-json_choice_validation_http_rabbitmq/src/main/java/com/basic01/create_route/ExampleRoute.java
@@ -0,0 +1,24 @@
+package com.basic01.create_route;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+import static org.apache.camel.LoggingLevel.INFO;
+
+@Component
+public class ExampleRoute extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		
+		// https://viacep.com.br/ws/01001000/json/
+		
+		from("timer:foo?period=1000")
+		.routeId("Main Route")
+		
+		.log(INFO, this.log, "This will be first message... maybe");
+		//.to("direct:named-http");
+	}
+
+		
+	
+}
