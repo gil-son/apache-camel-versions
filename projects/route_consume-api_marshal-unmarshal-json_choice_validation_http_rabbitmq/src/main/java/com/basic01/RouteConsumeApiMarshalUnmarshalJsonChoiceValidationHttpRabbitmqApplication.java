40061918b85c35b325cf39109e6faa93343ea0a0
“gil-son” <gilszon@gmail.com>
2021-09-26 18:13:02 -0300

    Initial

---
+++ projects/route_consume-api_marshal-unmarshal-json_choice_validation_http_rabbitmq/src/main/java/com/basic01/RouteConsumeApiMarshalUnmarshalJsonChoiceValidationHttpRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.basic01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteConsumeApiMarshalUnmarshalJsonChoiceValidationHttpRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteConsumeApiMarshalUnmarshalJsonChoiceValidationHttpRabbitmqApplication.class, args);
+	}
+
+}
---
+++ projects/route_consume-api_marshal-unmarshal-json_choice_validation_http_rabbitmq/src/test/java/com/basic01/RouteConsumeApiMarshalUnmarshalJsonChoiceValidationHttpRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteConsumeApiMarshalUnmarshalJsonChoiceValidationHttpRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
