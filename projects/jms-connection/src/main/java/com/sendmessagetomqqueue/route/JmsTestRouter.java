eab703f29015edbefdb76c970fa4da4c434e27e9
gilszon <gilszon@gmail.com>
2022-07-29 20:51:03 -0300

    JMS Connection

---
+++ projects/jms-connection/src/main/java/com/sendmessagetomqqueue/route/JmsTestRouter.java
@@ -0,0 +1,30 @@
+package com.sendmessagetomqqueue.route;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JmsTestRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        System.out.println("Configuring route");
+
+        from("{{input.queue}}")
+                .transform().constant("My Constant Message")
+                .log("${body}")
+                .log(LoggingLevel.DEBUG, log, "New message received")
+                .process(exchange -> {
+                    String convertedMessage = exchange.getMessage().getBody() + " is converted";
+                    exchange.getMessage().setBody(convertedMessage);
+                })
+                .to("{{output.queue}}")
+                .log(LoggingLevel.DEBUG, log, "Message sent to the other queue")
+        .end();
+
+    }
+}
